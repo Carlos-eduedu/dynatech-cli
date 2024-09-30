354d87a9c92d99b2401044c1bd900eae3df3b851
Carlos Eduardo Ferreira Fernandes <carlos@dynatech.tec.br>
2024-09-30 12:14:17 -0400

    implementar a estrutura base da CLI

---
+++ dynatech_cli/cli.py
@@ -0,0 +1,43 @@
+from rich.console import Console
+from typer import Context, Exit, Option, Typer
+from typing_extensions import Annotated
+
+from dynatech_cli import __version__
+
+console = Console()
+app = Typer()
+
+
+def version_func(flag: bool):
+    if flag:
+        console.print(f"Dynatech CLI version: {__version__}")
+        raise Exit()
+
+
+@app.callback(invoke_without_command=True)
+def main(
+    ctx: Context,
+    _: Annotated[
+        bool,
+        Option(
+        "--version",
+        "-v",
+        callback=version_func,
+        is_flag=True,
+        help="Mostre a versão do Dynatech CLI."
+        ),
+    ] = False
+):
+    message = """
+    Bem-vindo ao Dynatech CLI! 🚀
+
+    Para ver a lista de comandos disponíveis, execute:
+
+    $ dynatech --help
+    """
+
+    if ctx.invoked_subcommand:
+        return
+
+    console.print(message)
+    raise Exit()
